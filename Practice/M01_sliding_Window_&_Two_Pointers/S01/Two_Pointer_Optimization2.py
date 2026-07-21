df2edfbe34b2bb8ad8be7ab69e6d6a1a7d9b782d
Avaneesh <avaneeshvarma25@gmail.com>
2026-07-21 14:21:10 +0530

    2

--- Practice/M01_sliding_Window_&_Two_Pointers/S01/Two_Pointer_Optimization2.py
+++ Practice/M01_sliding_Window_&_Two_Pointers/S01/Two_Pointer_Optimization2.py
@@ -1,11 +1,14 @@
 from typing import List
-def removeDuplicates(nums: List[int]) -> int:
+def removeElement(nums: List[int], val: int) -> int:
         i = 0
-        for j in range(1,len(nums)):
-            if nums[i] != nums[j]:
-                i += 1
+        for j in range(0,len(nums)):
+            if nums[j] != val:
+                
                 nums[i] = nums[j]
-        return i+1        
-        
+                i += 1
+                
+        return i   
+
 nums = [0,0,1,1,1,2,2,3,3,4]
-print(removeDuplicates(nums))
+val = 1
+print(removeElement(nums, val))
